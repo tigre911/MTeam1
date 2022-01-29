90e6bc7b11b56fcf5e330fea11dada8e60b1da48
tigre911 <94420939+tigre911@users.noreply.github.com>
2022-01-30 02:04:51 +0900

    [Solved😁] BOJ_2839_김진우

--- 1월31일_그리디/BOJ_2839_김진우.py
+++ 1월31일_그리디/BOJ_2839_김진우.py
@@ -1,15 +1,24 @@
 import sys
 input = sys.stdin.readline
 
+#설탕 N 킬로그램
 n = int(input())
-sugar_list = [5,3]
 
 count = 0
 
-for i in sugar_list:
-    count += n//i   #1. 5로 나눈 몫을 더하고            2. 3으로 나눈 몫을 더함
-    n %= i          #1. 5로 나눈 나머지를 n에 다시 대입
-    if n > 0 :      #나머지가 0보다 크면 정확하게 N킬로그램을 만들 수 없는 것
-        count = -1
-
-print(count)
+if n % 5 == 0:  # 5으로 나눠떨어질 때
+    print(n // 5)
+else:           # 5으로 나눠떨어지지 않을 때
+    while n > 0:
+        n -= 3          # 3kg 한봉을 담아서 전체 무게에서 3키로를 빼주고 5키로로 다되는지 확인하는 과정을 거침
+        count += 1
+        if n % 5 == 0:  # 3kg과 5kg를 조합해서 담을 수 있을 때
+            count += n // 5
+            print(count)
+            break
+        elif n == 1 or n == 2:  # 설탕 봉지만으로 나눌 수 없을 때 3이하 1,2일때 나눠지지 않음
+            print(-1)
+            break
+        elif n == 0:  # 3으로 나눠떨어질 때
+            print(count)
+            break
