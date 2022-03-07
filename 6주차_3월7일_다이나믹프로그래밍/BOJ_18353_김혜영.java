446b9b4fcc5309a243ef3365cfb55ede53a74929
Hyeyoung Kim <hyeyoung.dev@gmail.com>
2022-03-07 22:25:45 +0900

    [Solved😁] 김혜영_병사 배치하기

---
+++ 6주차_3월7일_다이나믹프로그래밍/BOJ_18353_김혜영.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_18353_병사_배치하기 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	       int N = Integer.parseInt(br.readLine());
+	       
+	       int[] soldiers = new int[N+1];
+	       StringTokenizer st = new StringTokenizer(br.readLine());
+	       for(int i=1; i<=N; i++) {
+	    	   soldiers[i] = Integer.parseInt(st.nextToken());
+	       }
+	       
+	       int[] dp = new int[N+1];
+	       Arrays.fill(dp, 1);
+	       
+	       for(int i=1; i<=N; i++){
+	            for(int j=1; j<i; j++){
+	                if(soldiers[j]> soldiers[i]){
+	                    dp[i] = Math.max(dp[j]+1, dp[i]);
+	                }
+	            }
+	        }
+
+	        int max = 0;
+	        for(int i=1; i<=N; i++){
+	            max = Math.max(max, dp[i]);
+	        }
+
+	        System.out.println(N-max);
+	}
+	
+}
