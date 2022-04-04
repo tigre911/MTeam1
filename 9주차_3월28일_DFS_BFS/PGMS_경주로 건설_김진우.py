09814d35414fc7344f481f06d7a006574420286a
tigre911 <94420939+tigre911@users.noreply.github.com>
2022-04-04 21:06:45 +0900

    [😁sovled]김진우_경주로건설

--- 9주차_3월28일_DFS_BFS/PGMS_경주로 건설_김진우.py
+++ 9주차_3월28일_DFS_BFS/PGMS_경주로 건설_김진우.py
@@ -1,39 +1,57 @@
+import sys
 from collections import deque
-
+ 
+ 
 def solution(board):
-    answer = 0
-    answer = 999999
+    # 상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     q = deque()
-    q.append((0, 0, 4, 0))
-
-    visited = {  
-        (0, 0, 1): 0,  # (x, y, 현재위치(x, y)에서 바라보고 있는 방향): 현재위치까지 오는데 드는 비용
-        (0, 0, 3): 0,  # 0, 1, 2, 3 | 상 하 좌 우
-    }
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    visit = [[sys.maxsize] * len(board) for _ in range(len(board))]
+    visit[0][0] = 0
+ 
+    # 현재 행, 현재 열, 상태(모름 : 0 / 가로 : 1 / 세로 : 2) , 금액
+    q.append([0, 0, 0, 0])
+ 
     while q:
-        x, y, d, c = q.popleft()
-
-        # 마지막에 도달 했을 때 최솟값을 결과에 넣는다.
-        if x == len(board) - 1 and y == len(board) - 1:
-            answer = min(answer, c)
-
-        for k in range(len(dx)):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < len(board) and 0 <= ny < len(board) and board[nx][ny] == 0:
-                nc = c
-                if d == 4:  # 맨처음엔 어느방향으로나 올 수 있으므로 직선도로 한다.
-                    nc += 100
-                elif d <= 1 and k <= 1: # 바라보는 방향과 진행 방향이 세로일 때
-                    nc += 100
-                elif d >= 2 and k >= 2: # 바라보는 방향과 진행 방향이 가로일 때
-                    nc += 100
-                else: # 바라보는 방향과 진행 방향이 서로 다를 때 | 코너와 직선이 생기므로 600원
-                    nc += 500 + 100
-                # 방문한 적이 없거나, 방문한 적이 있어도 기존의 비용보다 지금 온 경로의 비용(nc))가 적다면
-                if not visited.get((nx, ny, k)) or visited[(nx, ny, k)] > nc:
-                    visited[(nx, ny, k)] = nc  # 배열에 추가하거나, nc를 갱신해 준다.
-                    q.append((nx, ny, k, nc))  # 다음 출발 지를 q에 넣어준다.
-    
-    return answer
+ 
+        x, y, state, cnt = q.popleft()
+ 
+        for i in range(4):
+ 
+            nx = x + dx[i]
+            ny = y + dy[i]
+ 
+            if nx < 0 or ny < 0 or nx >= len(board) or ny >= len(board):
+                continue
+ 
+            if board[nx][ny] != 1:
+                # 가로상태 좌우 이동 -> 직진도로
+                if (state == 1 and (i == 2 or i == 3)) or (state == 2 and (i == 0 or i == 1)):
+                    if visit[nx][ny] >= cnt + 100:
+                        if state == 1:
+                            q.append([nx, ny, 1, cnt + 100])
+                            visit[nx][ny] = cnt + 100
+                        else:
+                            q.append([nx, ny, 2, cnt + 100])
+                            visit[nx][ny] = cnt + 100
+                elif (state == 1 and (i == 0 or i == 1)) or (state == 2 and (i == 2 or i == 3)):
+                    if visit[nx][ny] >= cnt + 600:
+                        if state == 1:
+                            q.append([nx, ny, 2, cnt + 600])
+                            visit[nx][ny] = cnt + 600
+                        else:
+                            q.append([nx, ny, 1, cnt + 600])
+                            visit[nx][ny] = cnt + 600
+ 
+                    # state 가 0인 상태일때
+                else:
+                    # 상하
+                    if i == 0 or i == 1:
+                        q.append([nx, ny, 2, 100])
+                        visit[nx][ny] = 100
+                    else:
+                        q.append([nx, ny, 1, 100])
+                        visit[nx][ny] = 100
+ 
+    return visit[len(board) - 1][len(board) - 1]
