521325550e49a4c1db79ab104936dae878fa41d3
tigre911 <94420939+tigre911@users.noreply.github.com>
2022-04-04 20:47:20 +0900

    [solved😁]김진우_경주로건설

--- 9주차_3월28일_DFS_BFS/PGMS_경주로 건설_김진우.py
+++ 9주차_3월28일_DFS_BFS/PGMS_경주로 건설_김진우.py
@@ -1,29 +1,39 @@
-import sys
 from collections import deque
 
 def solution(board):
-    def bfs(start):
-        answer = sys.maxsize
-        dic = {0:[-1, 0], 1:[0, 1], 2:[1, 0], 3:[0, -1]} # 북동남서
-        length = len(board)
-        visited = [[sys.maxsize]*length for _ in range(length)]
-        visited[0][0] = 0
+    answer = 0
+    answer = 999999
+    q = deque()
+    q.append((0, 0, 4, 0))
 
-        q = deque([start]) # x, y, cost, dir
-        while q:
-            x, y, c, d = q.popleft()
-            for i in range(4):
-                nx = x + dic[i][0]
-                ny = y + dic[i][1]
+    visited = {  
+        (0, 0, 1): 0,  # (x, y, 현재위치(x, y)에서 바라보고 있는 방향): 현재위치까지 오는데 드는 비용
+        (0, 0, 3): 0,  # 0, 1, 2, 3 | 상 하 좌 우
+    }
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    while q:
+        x, y, d, c = q.popleft()
 
-                # 범위안에 들어오는지 확인 
-                if 0 <= nx < length and 0 <= ny < length and board[nx][ny] == 0:
-                    # 진행방향에 따른 비용 추가
-                    nc = c+100 if i == d else c+600
-                    if nc < visited[nx][ny]:
-                        visited[nx][ny] = nc
-                        q.append([nx, ny, nc, i])
-                        
-        return visited[-1][-1]
+        # 마지막에 도달 했을 때 최솟값을 결과에 넣는다.
+        if x == len(board) - 1 and y == len(board) - 1:
+            answer = min(answer, c)
+
+        for k in range(len(dx)):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < len(board) and 0 <= ny < len(board) and board[nx][ny] == 0:
+                nc = c
+                if d == 4:  # 맨처음엔 어느방향으로나 올 수 있으므로 직선도로 한다.
+                    nc += 100
+                elif d <= 1 and k <= 1: # 바라보는 방향과 진행 방향이 세로일 때
+                    nc += 100
+                elif d >= 2 and k >= 2: # 바라보는 방향과 진행 방향이 가로일 때
+                    nc += 100
+                else: # 바라보는 방향과 진행 방향이 서로 다를 때 | 코너와 직선이 생기므로 600원
+                    nc += 500 + 100
+                # 방문한 적이 없거나, 방문한 적이 있어도 기존의 비용보다 지금 온 경로의 비용(nc))가 적다면
+                if not visited.get((nx, ny, k)) or visited[(nx, ny, k)] > nc:
+                    visited[(nx, ny, k)] = nc  # 배열에 추가하거나, nc를 갱신해 준다.
+                    q.append((nx, ny, k, nc))  # 다음 출발 지를 q에 넣어준다.
     
-    return min([bfs((0, 0, 0, 1)), bfs((0, 0, 0, 2))])
+    return answer
